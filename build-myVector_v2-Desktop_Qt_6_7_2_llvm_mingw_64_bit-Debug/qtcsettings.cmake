12122eb21e309188a2025280a53f013fabdcb680
BRSPD-GKundi <Gurpal.Kundi@brightspeed.com>
2024-10-06 07:44:09 -0700

    latest commit

---
+++ build-myVector_v2-Desktop_Qt_6_7_2_llvm_mingw_64_bit-Debug/qtcsettings.cmake
@@ -0,0 +1,10 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "C:/Users/p4u1/Documents/MIT_Projects/Projects/build-myVector_v2-Desktop_Qt_6_7_2_llvm_mingw_64_bit-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "C:/Qtv1/6.7.2/llvm-mingw_64/bin/qmake.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PREFIX_PATH" "C:/Qtv1/6.7.2/llvm-mingw_64" CACHE "PATH" "" FORCE)
+set("CMAKE_C_COMPILER" "C:/Qtv1/Tools/llvm-mingw1706_64/bin/clang.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_COMPILER" "C:/Qtv1/Tools/llvm-mingw1706_64/bin/clang++.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
